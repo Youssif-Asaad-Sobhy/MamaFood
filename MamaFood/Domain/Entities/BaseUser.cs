e93eb4aa317a00a1f8cc85389e7152c49e673a4e
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2024-02-02 18:20:34 +0200

    LOL

---
+++ MamaFood/Domain/Entities/BaseUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MamaFood.API.Domain.Entities
+{
+    public class BaseUser:IdentityUser
+    {
+        [Required]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        public string? NID { get; set; }
+        public string? Describtin { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public byte[]? Photo { get; set; }
+    }
+}
